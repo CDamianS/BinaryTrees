387aaba1c648ffdefb0d13471f3ee2b638217338
CDamianS <A01656277@tec.mx>
2022-12-08 22:23:26 -0600

    Fixed formatting

--- main.cpp
+++ main.cpp
@@ -10,40 +10,40 @@ using namespace std;
 #include "hashtablevd.h"
 #include "bst.h"
 
+int main(int argc, char *argv[]) {
+  ifstream archivo(NOMBRE_ARCHIVO);
+  string linea;
+  char delimitador = ',';
+  // Leemos la primer línea para descartarla, pues es el encabezado
+  getline(archivo, linea);
+  // Leemos todas las líneas
+  BST<string> bst;
+  while (getline(archivo, linea)) {
+    stringstream stream(linea); // Convertir la cadena a un  stream
+    string id, first_name, last_name, gender, dob, phone, email, ZIP, city,
+        state;
+    // Extraer todos los valores de esa fila
 
-int main(int argc, char * argv[]) {
-   ifstream archivo(NOMBRE_ARCHIVO);
-   string linea;
-   char delimitador = ',';
-   // Leemos la primer línea para descartarla, pues es el encabezado
-   getline(archivo, linea);
-   // Leemos todas las líneas
-   BST<string> bst;  
-   while (getline(archivo, linea)) {
-     stringstream stream(linea); // Convertir la cadena a un  stream
-     string id,first_name,last_name,gender,dob,phone,email,ZIP,city,state;
-     // Extraer todos los valores de esa fila
-     
-     getline(stream, id, delimitador);
-     getline(stream, first_name, delimitador);
-     getline(stream, last_name, delimitador);
-     getline(stream, gender, delimitador);
-     getline(stream, dob, delimitador);
-     getline(stream, phone, delimitador);
-     getline(stream, email, delimitador);
-     getline(stream, ZIP, delimitador);
-     getline(stream, city, delimitador);
-     getline(stream, state, delimitador);
-     //Persona persona(id, ZIP, first_name, last_name, city);
-     //BST
-     bst.insert(city);
-   }
-   //cout << bst << endl;
-   
-   cout << "root: " << bst.getroot() << endl;
-   bst.remove("Marina");
-   cout << "root: " << bst.getroot() << endl;
-   bst.ComparadorHeight();
-   // bst.inOrden();
-   return 0;
+    getline(stream, id, delimitador);
+    getline(stream, first_name, delimitador);
+    getline(stream, last_name, delimitador);
+    getline(stream, gender, delimitador);
+    getline(stream, dob, delimitador);
+    getline(stream, phone, delimitador);
+    getline(stream, email, delimitador);
+    getline(stream, ZIP, delimitador);
+    getline(stream, city, delimitador);
+    getline(stream, state, delimitador);
+    // Persona persona(id, ZIP, first_name, last_name, city);
+    // BST
+    bst.insert(city);
+  }
+  // cout << bst << endl;
+
+  cout << "root: " << bst.getroot() << endl;
+  bst.remove("Marina");
+  cout << "root: " << bst.getroot() << endl;
+  bst.ComparadorHeight();
+  // bst.inOrden();
+  return 0;
 }
